861e482935194a871b699386af40153e9cf12652
VickyBhaskar <vicky2400kumar@gmail.com>
2023-12-20 21:58:39 +0530

    initial Commit

---
+++ SQLQuery.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE ProductInvendb
+USE ProductInvendb
+CREATE TABLE [dbo].[Products] (
+    [ProductId] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+    [ProductName] NVARCHAR (50) NOT NULL,
+    [Price] FLOAT NOT NULL,
+    [Quantity] INT NOT NULL,
+    [MfDate] DATE NOT NULL,
+    [ExpDate] DATE NOT NULL
+)
